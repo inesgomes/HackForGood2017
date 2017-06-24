6eba19c0af00c5e14a162904ad8990047c828ea9
miguelagfreitas <miguel.agfreitas@gmail.com>
2017-06-24 22:13:36 +0100

    correcoes v2

--- app/server.py
+++ app/server.py
@@ -31,35 +31,35 @@ class WebApp(object):
 
     @cherrypy.expose
     def listall(self):
-        return open("app/listall.html")
+        return open("app/views/listall.html")
 
     @cherrypy.expose
     def login(self):
-        return open("app/login.html")
+        return open("app/views/login.html")
 
     @cherrypy.expose
     def main(self):
-        return open("app/main.html")
+        return open("app/views/main.html")
 
     @cherrypy.expose
     def profile(self):
-        return open("app/profile.html")
+        return open("app/views/profile.html")
 
     @cherrypy.expose
     def sendMessage(self):
-        return open("app/sendMessage.html")
+        return open("app/views/sendMessage.html")
 
     @cherrypy.expose
     def signup(self):
-        return open("app/signup.html")
+        return open("app/views/signup.html")
 
     @cherrypy.expose
     def ticket(self):
-        return open("app/ticket.html")
+        return open("app/views/ticket.html")
 
     @cherrypy.expose
     def ticketView(self):
-        return open("app/ticketView.html")
+        return open("app/views/ticketView.html")
 
     @cherrypy.expose
     def listAllUsers(self):
@@ -237,10 +237,6 @@ if __name__ == '__main__':
                "tools.staticdir.dir": "font-awesome"},
     }
 
-    cherrypy.config.update({'tools.sessions.on': True,
-                        'tools.sessions.storage_class': "File",
-                        'tools.sessions.storage_path': 'sessions',
-                        'tools.sessions.timeout': 10
-               })
+
     #cherrypy.server.socket_host='::'
     cherrypy.quickstart(WebApp(), '/', conf)
