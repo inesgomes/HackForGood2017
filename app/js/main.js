d1256f5e7aa095356933977f3c0abab0362961fa
Mário Fernandes <mgustavofernandes@gmail.com>
2017-06-24 22:53:47 +0100

    main

---
+++ app/js/main.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    listContent();
+}
+
+function listContent() {
+    $.ajax({
+        type: 'get',
+        url: '/listAllPedidos',
+        data: {},
+        success: function (data) {
+            var text = '<li class="info-element" id="task1">Simply dummy text of the printing and typesetting industry. ' +
+                '<div class="row"> ' +
+                '<div class="agile-detail col-lg-2"> ' +
+                '<strong>AUTOR</strong> ' +
+                '</div> ' +
+                '<div class="agile-detail col-lg-3"> ' +
+                '<i class="fa fa-clock-o"></i> DATA ' +
+                '</div> ' +
+                '<div class="agile-detail col-lg-4"> ' +
+                '<i class="fa fa-map-marker"></i> LOCALIZACAO ' +
+                '</div></div> </li>';
+
+            var text2 = '<li class="success-element" id="task9">' +
+                'Quisque venenatis ante in porta suscipit. <div class="row"> <div class="agile-detail col-lg-2"> ' +
+                '<strong>AUTOR</strong> ' +
+                '</div> ' +
+                '<div class="agile-detail col-lg-3"> ' +
+                '<i class="fa fa-clock-o"></i> DATA ' +
+                '</div> ' +
+                '<div class="agile-detail col-lg-4"> <i class="fa fa-map-marker"></i> LOCALIZACAO ' +
+                '</div> </div> </li>';
+
+            $("#offerList").append(text);
+            $("#helpList").append(text2);
+        }
+    });
+}
