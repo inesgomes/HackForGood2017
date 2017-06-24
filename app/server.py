adf0b1c9fcd05d33027106af583facb1767f54cd
Miguel Freitas <miguel.agfreitas@gmail.com>
2017-06-24 21:54:27 +0100

    api methods

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ import os, os.path
 import random
 import string
 import sqlite3 as sql
+import json
 
 import cherrypy
 
@@ -10,20 +11,201 @@ db = cur.cursor()
 
 
 class WebApp(object):
+
     @cherrypy.expose
     def index(self):
-        return open('index.html')
+        return open("index.html")
+
+    @cherrypy.expose
+    def listall(self):
+        return open("app/listall.html")
+
+    @cherrypy.expose
+    def login(self):
+        return open("app/login.html")
+
+    @cherrypy.expose
+    def main(self):
+        return open("app/main.html")
+
+    @cherrypy.expose
+    def profile(self):
+        return open("app/profile.html")
+
+    @cherrypy.expose
+    def sendMessage(self):
+        return open("app/sendMessage.html")
+
+    @cherrypy.expose
+    def signup(self):
+        return open("app/signup.html")
+
+    @cherrypy.expose
+    def ticket(self):
+        return open("app/ticket.html")
+
+    @cherrypy.expose
+    def ticketView(self):
+        return open("app/ticketView.html")
+
+    @cherrypy.expose
+    def listAllUsers(self):
+        result = db.execute('select * from users')
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['username'] = result[0]
+            dictlist[i]['email'] = result[1]
+            dictlist[i]['password'] = result[2]
+            dictlist[i]['telemovel'] = result[3]
+            dictlist[i]['localizacao'] = result[4]
+            i+=1
+        return json.dumps(dictlist)
+
+    @cherrypy.expose
+    def listAllPedidos(self):
+        result = db.execute('select * from pedidos')
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['username'] = result[1]
+            dictlist[i]['categoria'] = result[2]
+            dictlist[i]['titulo'] = result[3]
+            dictlist[i]['descricao'] = result[4]
+            dictlist[i]['localizacao'] = result[5]
+            dictlist[i]['time_stamp'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
+
+    @cherrypy.expose
+    def listAllOfertas(self):
+        result = db.execute('select * from ofertas')
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['username'] = result[1]
+            dictlist[i]['categoria'] = result[2]
+            dictlist[i]['titulo'] = result[3]
+            dictlist[i]['descricao'] = result[4]
+            dictlist[i]['localizacao'] = result[5]
+            dictlist[i]['time_stamp'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
+
+    @cherrypy.expose
+    def listAllMensagens(self):
+        result = db.execute('select * from mensagens')
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['recetor'] = result[1]
+            dictlist[i]['dador'] = result[2]
+            dictlist[i]['oferta'] = result[3]
+            dictlist[i]['pedido'] = result[4]
+            dictlist[i]['time_stamp'] = result[5]
+            dictlist[i]['mensagem'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
+
+    @cherrypy.expose
+    def addUser(self, username, email, password, telemovel, localizacao):
+        db.execute('insert into users(username, email, password, telemovel, localizacao) values (?,?,?,?,?)',
+        (username, email, password, telemovel, localizacao))
+        cur.commit()
+
+    @cherrypy.expose
+    def addPedido(self, username, categoria, titulo, descricao, localizacao):
+        db.execute('insert into pedidos(username, categoria, titulo, descricao, localizacao) values (?,?,?,?,?)',
+        (username, categoria, titulo, descricao, localizacao))
+        cur.commit()
+
+    @cherrypy.expose
+    def addOferta(self, username, categoria, titulo, descricao, localizacao):
+        db.execute('insert into ofertas(username, categoria, titulo, descricao, localizacao) values (?,?,?,?,?)',
+        (username, categoria, titulo, descricao, localizacao))
+        cur.commit()
+
+    @cherrypy.expose
+    def addMensagem(self, recetor, dador, oferta, pedido, mensagem):
+        db.execute('insert into mensagens(recetor, dador, oferta, pedido, mensagem) values (?,?,?,?,?)',
+        (recetor, dador, oferta, pedido, mensagem))
+        cur.commit()
+
+    @cherrypy.expose
+    def getEventsByUsername(self, username):
+        result = db.execute('select * from pedidos where pedidos.username = ? join select * from ofertas where ofertas.username = ?',(username, username))
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['username'] = result[1]
+            dictlist[i]['categoria'] = result[2]
+            dictlist[i]['titulo'] = result[3]
+            dictlist[i]['descricao'] = result[4]
+            dictlist[i]['localizacao'] = result[5]
+            dictlist[i]['time_stamp'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
 
 
-@cherrypy.expose
-class WebAppService(object):
+    @cherrypy.expose
+    def getOfertasById(self. id):
+        result = db.execute('select * from ofertas where ofertas.id = ?',(id))
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['username'] = result[1]
+            dictlist[i]['categoria'] = result[2]
+            dictlist[i]['titulo'] = result[3]
+            dictlist[i]['descricao'] = result[4]
+            dictlist[i]['localizacao'] = result[5]
+            dictlist[i]['time_stamp'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
 
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        return cherrypy.session['mystring']
+    @cherrypy.expose
+    def getPedidosById(self. id):
+        result = db.execute('select * from pedidos where ofertas.id = ?',(id))
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['username'] = result[1]
+            dictlist[i]['categoria'] = result[2]
+            dictlist[i]['titulo'] = result[3]
+            dictlist[i]['descricao'] = result[4]
+            dictlist[i]['localizacao'] = result[5]
+            dictlist[i]['time_stamp'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
 
-
-
+    @cherrypy.expose
+    def getReceivedMensagensByUser(self, username):
+        result = db.execute('select * from mensagens where mensagens.receiver = ?', (username))
+        data = result.fetchall()
+        dictlist = [dict() for x in range(len(data))]
+        i = 0
+        for result in data:
+            dictlist[i]['id'] = result[0]
+            dictlist[i]['recetor'] = result[1]
+            dictlist[i]['dador'] = result[2]
+            dictlist[i]['oferta'] = result[3]
+            dictlist[i]['pedido'] = result[4]
+            dictlist[i]['time_stamp'] = result[5]
+            dictlist[i]['mensagem'] = result[6]
+            i+=1
+        return json.dumps(dictlist)
 
 if __name__ == '__main__':
     conf = {
@@ -39,8 +221,13 @@ if __name__ == '__main__':
     "/fonts": {"tools.staticdir.on": True,
                "tools.staticdir.dir": "fonts"},
     "/font-awesome": {"tools.staticdir.on": True,
-               "tools.staticdir.dir": "font-awesome"}}
+               "tools.staticdir.dir": "font-awesome"},
+    {'tools.sessions.on': True,
+                        'tools.sessions.storage_type': "File",
+                        'tools.sessions.storage_path': 'sessions',
+                        'tools.sessions.timeout': 10
+               }}
 
-    webapp = WebApp()
-    cherrypy.server.socket_host='::'
-    cherrypy.quickstart(webapp, '/', conf)
+
+    #cherrypy.server.socket_host='::'
+    cherrypy.quickstart(WebApp(), '/', conf)
--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
 create table users(
 	username text,
-	contacto text,
+	email text,
+	password text,
+	telemovel text,
+	localizacao text,
 	primary key(username)
 );
 
@@ -8,26 +11,28 @@ create table pedidos(
 	id SERIAL PRIMARY KEY,
 	username text REFERENCES users (username),
 	categoria text,
-	time_stamp timestamp,
-	custo text
+	titulo text,
+	descricao text,
+	localizacao text,
+	time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 create table ofertas(
 	id SERIAL PRIMARY KEY,
 	username text REFERENCES users (username),
 	categoria text,
-	time_stamp timestamp,
-	custo text
+	titulo text,
+	descricao text,
+	localizacao text,
+	time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table resposta(
-	id SERIAL PRIMARY KEY,
-	recetor text REFERENCES users (username),
-	dador text REFERENCES users (username),
-	time_stamp timestamp,
-	comunicacao text
-);
-	
-	
-	
-	
+create table mensagens(
+	id serial primary key,
+	sender text references users(username),
+	receiver text references users(username),
+	oferta serial references ofertas(id),
+	pedido serial references pedidos(id),
+	time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+	mensagem text
+)
