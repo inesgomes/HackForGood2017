b8a110f6a4deba80dc638ae36c76461b36c6c456
Mário Fernandes <mgustavofernandes@gmail.com>
2017-06-25 05:09:06 +0100

    update listall

--- app/js/listall.js
+++ app/js/listall.js
@@ -1,43 +1,63 @@
 window.onload = function () {
-listContent();
+    var ticket = getURLParameter('type');
+    if(ticket == "offer"){
+        listContentOffer();
+    } else if(ticket == "request") {
+        listContentRequest();
+    }
 }
 
-function listContent() {
+function listContentOffer() {
+    $.ajax({
+        type: 'get',
+        url: '/listAllOfertas',
+        data: {},
+        success: function (data) {
+            data = JSON.parse(data);
+            listContent(data);
+        }
+    });
+}
+
+function listContentRequest(){
     $.ajax({
         type: 'get',
         url: '/listAllPedidos',
         data: {},
         success: function (data) {
-            console.log(data);
             data = JSON.parse(data);
-            var col1 = "";
-            var col2 = "";
-            var col3 = "";
-            for (var i = 0; i < data.length; i++){
-                var text = '<div class="col-lg-4">' +
-                    '<div class="ibox">' +
-                    '<div class="ibox-content">' +
-                    '<a href="article.html" class="btn-link">' +
-                    '<h2>'+data[i].titulo+'</h2>' +
-                    '</a>' +
-                    '<div class="small m-b-xs">' +
-                    '<strong>'+data[i].username+'</strong>' +
-                    '<span class="text-muted">' +
-                    '<i class="fa fa-clock-o"></i>'+data[i].time_stamp+'<i class="fa fa-map-marker"></i>'+data[i].localizacao+'</span> </div>' +
-                    '<p>'+data[i].descricao+'</p>' +
-                    '<div class="text-right"> <button class="btn btn-white btn-xs" type="button">Match/help</button></div>' +
-                    '</div> </div> </div>';
-                if(i%3 == 0){
-                    col1 = col1.concat(text);
-                } else if (i%3 == 1) {
-                    col2 = col2.concat(text);
-                } else if (i%3 == 2){
-                    col3 = col3.concat(text);
-                }
-            }
-            $("#row-ticket").append(col1);
-            $("#row-ticket").append(col2);
-            $("#row-ticket").append(col3);
+            listContent(data);
         }
     });
+}
+
+function listContent(data){
+    var col1 = "";
+    var col2 = "";
+    var col3 = "";
+    for (var i = 0; i < data.length; i++){
+        var text = '<div class="col-lg-4">' +
+            '<div class="ibox">' +
+            '<div class="ibox-content">' +
+            '<a href="article.html" class="btn-link">' +
+            '<h2>'+data[i].titulo+'</h2>' +
+            '</a>' +
+            '<div class="small m-b-xs">' +
+            '<strong>'+data[i].username+'</strong>' +
+            '<span class="text-muted">' +
+            '<i class="fa fa-clock-o"></i>'+data[i].time_stamp+'<i class="fa fa-map-marker"></i>'+data[i].localizacao+'</span> </div>' +
+            '<p>'+data[i].descricao+'</p>' +
+            '<div class="text-right"> <button class="btn btn-white btn-xs" type="button">Match/help</button></div>' +
+            '</div> </div> </div>';
+        if(i%3 == 0){
+            col1 = col1.concat(text);
+        } else if (i%3 == 1) {
+            col2 = col2.concat(text);
+        } else if (i%3 == 2){
+            col3 = col3.concat(text);
+        }
+    }
+    $("#row-ticket").append(col1);
+    $("#row-ticket").append(col2);
+    $("#row-ticket").append(col3);
 }
