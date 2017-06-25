abd454d760c622fe39ba8ffeb2a53331537c1d13
Mário Fernandes <mgustavofernandes@gmail.com>
2017-06-25 05:34:19 +0100

    main

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,52 @@
 window.onload = function () {
-    listContent();
+    listContentOffer();
 }
 
-function listContent() {
+function listContentOffer(){
+    $.ajax({
+        type: 'get',
+        url: '/listAllOfertas',
+        data: {},
+        success: function (data) {
+            data = JSON.parse(data);
+            $("#offerList").append(listText(data, "info"));
+            listContentRequest();
+        }
+    });
+}
+
+function listContentRequest(){
+
     $.ajax({
         type: 'get',
         url: '/listAllPedidos',
         data: {},
         success: function (data) {
             data = JSON.parse(data);
-            alert(data);
-            var text = '<li onclick="redirectOffer(1);" class="info-element" id="task1">Simply dummy text of the printing and typesetting industry. ' +
-                '<div class="row"> ' +
-                '<div class="agile-detail col-lg-2"> ' +
-                '<strong>AUTOR</strong> ' +
-                '</div> ' +
-                '<div class="agile-detail col-lg-3"> ' +
-                '<i class="fa fa-clock-o"></i> DATA ' +
-                '</div> ' +
-                '<div class="agile-detail col-lg-4"> ' +
-                '<i class="fa fa-map-marker"></i> LOCALIZACAO ' +
-                '</div></div> </li>';
-
-            var text2 = '<li onclick="redirectRequest(1);" class="success-element" id="task9">' +
-                'Quisque venenatis ante in porta suscipit. <div class="row"> <div class="agile-detail col-lg-2"> ' +
-                '<strong>AUTOR</strong> ' +
-                '</div> ' +
-                '<div class="agile-detail col-lg-3"> ' +
-                '<i class="fa fa-clock-o"></i> DATA ' +
-                '</div> ' +
-                '<div class="agile-detail col-lg-4"> <i class="fa fa-map-marker"></i> LOCALIZACAO ' +
-                '</div> </div> </li>';
-
-            $("#offerList").append(text);
-            $("#helpList").append(text2);
+            $("#helpList").append(listText(data, "success"));
         }
     });
+}
+
+function listText(data, color){
+
+    var text = '';
+
+    for(var i = 0; i < data.length; i++) {
+        text = text.concat('<li onclick="redirectOffer('+data[i].id+');" class="'+color+'-element" id="task1">'+data[i].titulo+' ' +
+            '<div class="row"> ' +
+            '<div class="agile-detail col-lg-2"> ' +
+            '<strong>'+data[i].username+'</strong> ' +
+            '</div>' +
+            '<div class="agile-detail col-lg-3"> ' +
+            '<i class="fa fa-clock-o"></i>'+data[i].time_stamp+'' +
+            '</div> ' +
+            '<div class="agile-detail col-lg-4"> ' +
+            '<i class="fa fa-map-marker"></i>'+data[i].localizacao+' ' +
+            '</div></div> </li>');
+    }
+
+    return text;
 }
 
 function redirectOffer(id){
